e92aa3a293fa43e685bda364dd8eefe168011ff3
PlaNexq <shio2001@ya.ru>
2022-05-27 01:27:34 +0500

    fixes

--- Assets/MathCourse/LookVector.cs
+++ Assets/MathCourse/LookVector.cs
@@ -5,30 +5,26 @@ using UnityEngine;
 public class LookVector : MonoBehaviour
 {
     [SerializeField]
-    private Transform lookPointTf;
-    [SerializeField]
     private Transform lookTargetTf;
     [SerializeField][Range(0f, 1f)]
     private float strictness = 1f;
 
+    private Vector2 lookDirection = Vector2.zero;
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
 
-        Vector2 lookPointPos = lookPointTf.position;
         Vector2 PlayerPos = transform.position;
         Vector2 LookTargetPos = lookTargetTf.position;
-
-        Vector2 lookVector = lookPointPos - PlayerPos;
-        float lookVectorMagnitude = Mathf.Sqrt(lookVector.x * lookVector.x + lookVector.y * lookVector.y);
-        Vector2 lookVectorNormalized = lookVector / lookVectorMagnitude;
 
         Vector2 lookTargetVector = LookTargetPos - PlayerPos;
         float lookTargetVectorMagnitude = Mathf.Sqrt(lookTargetVector.x * lookTargetVector.x + lookTargetVector.y * lookTargetVector.y);
         Vector2 lookTargetVectorNormalized = lookTargetVector / lookTargetVectorMagnitude;
 
-        float dot = lookVectorNormalized.x * lookTargetVectorNormalized.x + lookVectorNormalized.y * lookTargetVectorNormalized.y;
+        lookDirection = transform.right;
+
+        float dot = lookDirection.x * lookTargetVectorNormalized.x + lookDirection.y * lookTargetVectorNormalized.y;
 
         
         if(dot >= strictness)
@@ -36,6 +32,6 @@ public class LookVector : MonoBehaviour
             Gizmos.color = Color.green;
         }
 
-        Gizmos.DrawRay(PlayerPos, lookVector);
+        Gizmos.DrawRay(PlayerPos, lookDirection);
     }
 }
